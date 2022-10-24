365b417c110ae24d7f0b3d79256c14842ea41488
Rajesh-107 <applerb326@gmail.com>
2022-10-24 11:06:04 +0600

    task list redux state

--- src/redux/store/Store.js
+++ src/redux/store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SettingsReducer from "../slice/Settingslice";
+import taskReducer from "../slice/taskSlice";
 
 export default configureStore({
     reducer: {
         settings: SettingsReducer,
+        task:taskReducer
     }
 })
